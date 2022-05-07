c89e9cb59edda4945a3cc4f8e9fb6dc8cbc437eb
jeffreyccching <32418925+jeffreyccching@users.noreply.github.com>
2022-05-07 14:51:31 -0400

    first try

---
+++ Basics.v
@@ -0,0 +1,96 @@
+Inductive day : Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day 
+  | saturday : day
+  | sunday : day.
+
+(** The new type is called day, and its members are monday, tuesday, etc.
+Having defined day, we can write functions that operate on days.**)
+
+Definition next_weekday (d:day) : day :=
+  match d with
+  | monday    => tuesday
+  | tuesday   => wednesday
+  | wednesday => thursday
+  | thursday  => friday
+  | friday    => monday
+  | saturday  => monday
+  | sunday    => monday
+  end.
+
+(*Compute (next_weekday friday).
+Compute (next_weekday (next_weekday saturday)).*)
+(* ==> tuesday : day *)
+
+(*Example test_next_weekday:
+  (next_weekday (next_weekday saturday)) = tuesday.
+*)
+
+(*Proof. simpl. reflexivity.  Qed.*)
+
+Inductive bool : Type :=
+  | true : bool
+  | false : bool.
+
+Definition negb (b:bool)  : bool  :=
+  match b with
+  | true  =>  false
+  | false =>  true
+end.
+
+
+Definition andb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+Definition orb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.
+
+Example test_orb1:  (orb true  false) = true.
+Proof. simpl. reflexivity.  Qed.
+Example test_orb2:  (orb false false) = false.
+Proof. simpl. reflexivity.  Qed.
+Example test_orb3:  (orb false true)  = true.
+Proof. simpl. reflexivity.  Qed.
+Example test_orb4:  (orb true  true)  = true.
+Proof. simpl. reflexivity.  Qed.
+
+
+Notation "x && y" := (andb x y).
+Notation "x || y" := (orb x y).
+
+Example test_orb5:  false || false || true = true.
+Proof. simpl. reflexivity. Qed.
+
+Definition negb' (b:bool) : bool :=
+  if b then false
+  else true.
+Definition andb' (b1:bool) (b2:bool) : bool :=
+  if b1 then b2
+  else false.
+Definition orb' (b1:bool) (b2:bool) : bool :=
+  if b1 then true
+  else b2.
+
+
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+    | true => negb b2
+    | false =>  true
+  end.
+
+Example test_nandb1:               (nandb true false) = true.
+Proof. reflexivity. Qed.
+Example test_nandb2:               (nandb false false) = true.
+Proof. reflexivity. Qed.
+Example test_nandb3:               (nandb false true) = true.
+Proof. reflexivity. Qed.
+Example test_nandb4:               (nandb true true) = false.
+Proof. reflexivity. Qed.
